8da81c4a129fc9e77ffe1c022fdc1ef962021664
qjerome <qjerome@rawsec.lu>
2024-09-13 11:04:32 +0200

    feat(misp): add severity per IoC type

--- misp/misp-to-kunai.py
+++ misp/misp-to-kunai.py
@@ -2,29 +2,28 @@
 
 import os
 import sys
-import toml
 import time
 import json
-import logging
-import asyncio
-import urllib3
 import argparse
-import ipaddress
-import requests
 
 from typing import *
-from datetime import date, datetime, timezone, timedelta
+from datetime import date, datetime, timedelta
 
+import requests
+import toml
+import urllib3
 from pymisp import PyMISP, MISPEvent, MISPAttribute
 
-ALLOWED_TYPES = set([
-    "md5",
-    "sha1",
-    "sha256",
-    "domain",
-    "hostname",
-    "ip-dst",
-])
+TYPES_SEVERITY = {
+    "md5": 10,
+    "sha1": 10,
+    "sha256": 10,
+    "domain": 7,
+    "hostname": 7,
+    "ip-dst": 5,
+}
+
+REQUEST_TIMEOUT=10
 
 def log(msg: str, level:str):
     print(f"{level}:{msg}", file=sys.stderr)
@@ -44,17 +43,18 @@ def uuids_from_search(search):
 def iocs_from_attributes(source: str, attributes: List[MISPAttribute]) -> List[dict]:
     iocs = []
     for a in attributes:
-        if a.type in ALLOWED_TYPES:
-            iocs.append({"uuid": a.uuid, "source": source, "value": a.value, "event_uuid": a.event_uuid})
+        if a.type in TYPES_SEVERITY:
+            iocs.append({"type": a.type,
+            "uuid": a.uuid, 
+            "source": source, 
+            "value": a.value, 
+            "event_uuid": a.event_uuid, 
+            "severity": TYPES_SEVERITY[a.type]})
     return iocs
 
-def ioc_from_attribute(attr, source=""):
-    return IOC(uuid=attr.uuid, guuid=attr.event_uuid, source=source, value=attr.value, type=attr.type)
-
-
-def emit_attributes(misp: PyMISP, uuids: List[str]):
+def emit_attributes(py_misp: PyMISP, uuids: List[str]):
     for uuid in uuids:
-        event = misp.get_event(uuid, pythonify=True)
+        event = py_misp.get_event(uuid, pythonify=True)
         for attr in event_emit_attributes(event):
             yield attr
 
@@ -69,16 +69,16 @@ def event_emit_attributes(event: MISPEvent):
             attr.event_uuid = event.uuid
             yield attr
 
-def gen_kunai_iocs(misp: PyMISP, source:str, since: date, all: bool, tags=None):
+def gen_kunai_iocs(py_misp: PyMISP, source:str, since: date, all: bool, tags=None):
     published = True if not all else None
 
     # search events to pull attributes from
-    if since == None:
-        index = misp.search_index(published=published, tags=tags)
+    if since is None:
+        index = py_misp.search_index(published=published, tags=tags)
     else:
-        index = misp.search_index(published=published, timestamp=since, tags=tags)
+        index = py_misp.search_index(published=published, timestamp=since, tags=tags)
 
-    for attr in emit_attributes(misp, uuids_from_search(index)):
+    for attr in emit_attributes(py_misp, uuids_from_search(index)):
         for ioc in iocs_from_attributes(source, [attr]):
             yield ioc
 
@@ -87,13 +87,13 @@ def kunai_iocs_from_feed(feed_config: dict, since: date):
     if not url.endswith("manifest.json"):
         manifest_url = f"{url}/manifest.json"
 
-    manifest = requests.get(manifest_url).json()
+    manifest = requests.get(manifest_url, timeout=REQUEST_TIMEOUT).json()
 
     for event_uuid in manifest:
         event_ts = datetime.fromtimestamp(manifest[event_uuid]["timestamp"]).date()
         if event_ts < since:
             continue
-        event = requests.get(f"{url}/{event_uuid}.json").json()
+        event = requests.get(f"{url}/{event_uuid}.json", timeout=REQUEST_TIMEOUT).json()
         me = MISPEvent()
         me.from_dict(**event)
         for attr in event_emit_attributes(me):
@@ -131,7 +131,7 @@ if __name__ == "__main__":
     if args.silent:
         urllib3.disable_warnings()
 
-    config = toml.load(open(args.config))
+    config = toml.load(open(args.config, encoding="utf-8"))
 
     misp_config = config["misp"]
 
@@ -150,13 +150,13 @@ if __name__ == "__main__":
     if args.output != "/dev/stdout" and not args.overwrite:
         if os.path.isfile(args.output):
             info(f"building cache from existing file: {args.output}")
-            with open(args.output, "r") as fd:
+            with open(args.output, "r", encoding="utf-8") as fd:
                 for line in fd:
                     ioc = json.loads(line)
                     cache.add(ioc["uuid"])
 
-    open_mode = "w" if args.overwrite else "a"
-    with open(args.output, "a") as fd:
+    OPEN_MODE = "w" if args.overwrite else "a"
+    with open(args.output, "a", encoding="utf-8") as fd:
         while True:
             # processing events from a MISP instance
             if misp_config["enable"] is True:
